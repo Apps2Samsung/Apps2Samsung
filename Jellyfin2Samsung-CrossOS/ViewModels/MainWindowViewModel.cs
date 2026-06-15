fe126c76c883fe0be38e4e755dca50434933aa43
Patrick <patrick@madebypatrick.nl>
2026-06-15 15:23:55 +0200

    removed comment

--- Jellyfin2Samsung-CrossOS/ViewModels/MainWindowViewModel.cs
+++ Jellyfin2Samsung-CrossOS/ViewModels/MainWindowViewModel.cs
@@ -258,7 +258,7 @@ namespace Apps2Samsung.ViewModels
                 token.ThrowIfCancellationRequested();
 
                 SetStatus("ScanningNetwork");
-                //await LoadDevicesAsync(token);
+                await LoadDevicesAsync(token);
                 CustomWgtPath = AppSettings.Default.CustomWgtPath ?? "";
             }
             catch (OperationCanceledException)
@@ -414,7 +414,7 @@ namespace Apps2Samsung.ViewModels
                 if (!AvailableDevices.Any(d => d.IpAddress != L("lblOther")))
                 {
                     SetStatus("ScanningNetwork");
-                    //await LoadDevicesAsync(token);
+                    await LoadDevicesAsync(token);
                 }
 
                 CustomWgtPath = AppSettings.Default.CustomWgtPath ?? "";
