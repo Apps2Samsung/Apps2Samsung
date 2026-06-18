6e1d3a406520e61b330586f232b87389300a8421
Patrick <patrick@madebypatrick.nl>
2026-06-18 09:07:37 +0200

    Expect JSON else silent writing, no more errors

--- Jellyfin2Samsung-CrossOS/Helpers/AppSettings.cs
+++ Jellyfin2Samsung-CrossOS/Helpers/AppSettings.cs
@@ -118,7 +118,7 @@ namespace Apps2Samsung.Helpers
         [JsonIgnore]
         public string AuthorEndpoint { get; set; } = "https://dev.tizen.samsung.com/apis/v2/authors";
         [JsonIgnore]
-        public string AppVersion { get; set; } = "v2.5.5";
+        public string AppVersion { get; set; } = "v2.5.6";
         [JsonIgnore]
         public string TizenSdb { get; set; } = "https://api.github.com/repos/PatrickSt1991/tizen-sdb/releases";
         [JsonIgnore]
